5cdde2458136a99245f832b6569c607f6da0a2a5
Stephan Wiesenhütter <wiesenhuetter@gmail.com>
2022-12-21 06:31:22 +0100

    add mnist data
    remove mnist test data (too big)

--- nnet/ai.py
+++ nnet/ai.py
@@ -8,43 +8,55 @@ def sigmoid(x):
 def sigmoid_derivative(x):
     return x * (1 - x)
 
+def init_three_layer_net(in_data, out_data, hidden_units):
+    assert out_data.ndim == 2
+    num_out_units = out_data.shape[1]
+    assert in_data.ndim == 2
+    num_in_units = in_data.shape[1]
 
-# Input data
-X = np.array([[0, 0, 1],
-              [0, 1, 1],
-              [1, 0, 1],
-              [1, 1, 1]])
+    w1 = 2 * np.random.random((num_in_units, hidden_units)) - 1
+    w2 = 2 * np.random.random((hidden_units, num_out_units)) - 1
 
-# Output data
-y = np.array([[0], [1], [1], [0]])
+    return w1, w2
 
-# Seed the random number generator to get reproducible results
-np.random.seed(1)
 
-# Initialize weights randomly with mean 0
-W1 = 2 * np.random.random((3, 4)) - 1
-W2 = 2 * np.random.random((4, 1)) - 1
+if __name__ == '__main__':
+    # Input data
+    X = np.array([[0, 0, 1],
+                  [0, 1, 1],
+                  [1, 0, 1],
+                  [1, 1, 1]])
 
-# Set the learning rate
-learning_rate = 0.1
+    # Output data
+    y = np.array([[0], [1], [1], [0]])
 
-# Perform backpropagation
-for iteration in range(10000):
-    # Forward propagation
-    layer1 = X
-    layer2 = sigmoid(np.dot(layer1, W1))
-    layer3 = sigmoid(np.dot(layer2, W2))
+    # Seed the random number generator to get reproducible results
+    np.random.seed(1)
 
-    # Calculate the error
-    error = y - layer3
-    error_delta = error * sigmoid_derivative(layer3)
+    # Initialize weights randomly with mean 0
+    W1 = 2 * np.random.random((3, 5)) - 1
+    W2 = 2 * np.random.random((5, 1)) - 1
 
-    # Calculate the error for layer 2
-    layer2_error = error_delta.dot(W2.T)
-    layer2_delta = layer2_error * sigmoid_derivative(layer2)
+    # Set the learning rate
+    learning_rate = 0.1
 
-    # Update the weights
-    W2 += layer2.T.dot(error_delta) * learning_rate
-    W1 += layer1.T.dot(layer2_delta) * learning_rate
+    # Perform backpropagation
+    for iteration in range(50000):
+        # Forward propagation
+        layer1 = X
+        layer2 = sigmoid(np.dot(layer1, W1))
+        layer3 = sigmoid(np.dot(layer2, W2))
 
-print(f"Final output after training: {layer3}")
+        # Calculate the error
+        error = y - layer3
+        error_delta = error * sigmoid_derivative(layer3)
+
+        # Calculate the error for layer 2
+        layer2_error = error_delta.dot(W2.T)
+        layer2_delta = layer2_error * sigmoid_derivative(layer2)
+
+        # Update the weights
+        W2 += layer2.T.dot(error_delta) * learning_rate
+        W1 += layer1.T.dot(layer2_delta) * learning_rate
+
+    print(f"Final output after training: {layer3}")
---
+++ nnet/mnist.py
@@ -0,0 +1,28 @@
+import numpy as np
+from PIL import Image
+import sys
+
+
+def load_data():
+    data_set = np.loadtxt("..\\data\\mnist_test.csv", skiprows=1, dtype=np.uint8, delimiter=',')
+    return data_set
+
+
+def get_image(data_set, index):
+    img_data = data_set[index, 1:].reshape((28, 28))
+    label = data_set[index, 0]
+    img = Image.fromarray(img_data, 'L')
+    return label, img
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: python mnist.py <index>")
+        sys.exit(1)
+
+    index = int(sys.argv[1])
+
+    dataset = load_data()
+    label, img = get_image(dataset, index)
+    print(f"Label: {label}")
+    img.show()
